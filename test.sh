6512c3d55c86d945e06fc2f5da94d8fb18e4b312
eshamster <hamgoostar@gmail.com>
2015-11-14 01:28:37 +0900

    Add files for CircleCI
    	new file:   circle.yml
    	new file:   test.sh

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -u
+
+targets=$*
+
+res=0
+
+for target in ${targets[@]}; do
+    ros use ${target}
+    ros run -- --version
+    run-prove *-test.asd
+
+    temp=$?
+    if [ ${temp} -ne 0 ]; then
+        res=${temp}
+    fi
+done
+
+exit ${res}
